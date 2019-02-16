39d7852ed2feec6d83211ec9073a51128e290b0a
thepauljones <thepauljones@gmail.com>
2019-02-16 20:46:59 +0100

    webpack.config.js formatting

--- webpack.config.js
+++ webpack.config.js
@@ -27,23 +27,24 @@ module.exports = {
     ],
     module: {
         rules: [
-        {
-            test: /\.scss$/,
-            use: [
-                'style-loader', // creates style nodes from JS strings
-                'css-loader', // translates CSS into CommonJS
-                'sass-loader' // compiles Sass to CSS, using Node Sass by default
-            ]
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                  test: /\.m?js$/,
+                  exclude: /(node_modules)/,
+                  use: {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: ['@babel/preset-env']
+                      }
+                  }
             }
-          }
-        }]
+        ]
     }
 };
